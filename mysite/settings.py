99f65b3f56ad4755c6d5b5b48194c4fdb5f809fb
admin <admin@admin.com>
2017-06-22 18:01:58 +0530

    Bootstrap

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&f!oxcjk90%pp__#bu$yxktg!fyxh^=ox=vemw!)k(er81wuoz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
